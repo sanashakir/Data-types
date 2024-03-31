bb7b7e96b8a3586c379e63c0fa86a16ce6841753
Sana Shakir <161624394+sanashakir@users.noreply.github.com>
2024-03-31 16:17:44 +0500

    Add files via upload

---
+++ Data type/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Data type :-
+// 1."string" for textual data. who present in inverted comma ("Sana Shakir").
+let message = "Sana Shakir";
+// 2."number" for numarical values (16).
+let age = 16;
+// 3."boolean" for true/false values.
+let isMarried = false;
+// 4."any" for variables that can hold any type of data.
+let link = "https://www.linkedin.com/";
---
+++ Data type/main.ts
@@ -0,0 +1,13 @@
+// Data type :-
+
+// 1."string" for textual data. who present in inverted comma/double quotes ("Sana Shakir").
+let message : string = "Sana Shakir";
+
+// 2."number" for numarical values (8007).
+let rollNumber : number = 8007 ;
+
+// 3."boolean" for (true/false) values.
+let isHungry : boolean= false;
+
+// 4."any" for variables that can hold any type of data.
+let link : any = "https://www.linkedin.com/in/sana-shakir";
